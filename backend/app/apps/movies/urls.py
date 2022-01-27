5941f2b8956275ed8efbcf93207f786f701f7be6
samaelgomez <samaelgomezherrera@gmail.com>
2022-01-27 19:13:14 +0100

    Movies list API view

--- backend/app/apps/movies/urls.py
+++ backend/app/apps/movies/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from .views import MovieAdminViewSet
+from .views import (MovieAdminViewSet, MovieListAPIView)
 
 router = DefaultRouter()
 router.register(r'movies_admin', MovieAdminViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^movies/?$', MovieListAPIView.as_view()),
 ]
--- backend/app/apps/movies/views.py
+++ backend/app/apps/movies/views.py
@@ -10,3 +10,18 @@ class MovieAdminViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     lookup_field = 'slug'
     permission_classes = (IsAdminUser,)
+
+class MovieListAPIView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    pagination_class = None
+    permission_classes = (AllowAny,)
+    serializer_class = MovieSerializer
+
+    def list(self, request):
+        serializer_data = self.get_queryset()
+        serializer = self.serializer_class(serializer_data, many=True)
+
+        print(serializer.data)
+        return Response({
+            'movies': serializer.data
+        }, status=status.HTTP_200_OK)
