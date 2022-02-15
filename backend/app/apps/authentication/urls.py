d35616d245b080823eda6b0bcfeee5e193cf7d5d
samaelgomez <samaelgomezherrera@gmail.com>
2022-02-15 20:01:28 +0100

    Backend login implementation

--- backend/app/apps/authentication/urls.py
+++ backend/app/apps/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from .views import RegistrationAPIView
+from .views import LoginAPIView, RegistrationAPIView
 
 router = DefaultRouter()
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^users/?$', RegistrationAPIView.as_view()),
+    url(r'^users/login/?$', LoginAPIView.as_view()),
 ]
--- backend/app/apps/authentication/views.py
+++ backend/app/apps/authentication/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from .models import User
 from .serializers import (
-    RegistrationSerializer, UserSerializer
+    LoginSerializer, RegistrationSerializer, UserSerializer
 )
 
 
@@ -35,3 +35,20 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        # Notice here that we do not call `serializer.save()` like we did for
+        # the registration endpoint. This is because we don't actually have
+        # anything to save. Instead, the `validate` method on our serializer
+        # handles everything we need.
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
