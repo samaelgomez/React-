e817f4bdd99e7cb81817d918439f3fee3d67cf5c
samaelgomez <samaelgomezherrera@gmail.com>
2022-02-17 20:52:26 +0100

    Header fully implemented

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -3,26 +3,25 @@ import "./Header.css";
 
 export default function Header() {
     return (
-        <header className="page-header">
-            <div className="header-content">
-                <div className="logo">
+        <header className="pageHeader">
+            <div className="headerContent">
+                <div className="headerLogo">
                     <h1>AniRent</h1>
                 </div>
-                <input type="checkbox" id="hamburger-checkbox" />
-                <label htmlFor="hamburger-checkbox" className="hamburger">
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
+                <input type="checkbox" id="hamburgerCheckbox" />
+                <label htmlFor="hamburgerCheckbox" className="hamburger">
+                    <div className="hamburgerLine"></div>
+                    <div className="hamburgerLine"></div>
+                    <div className="hamburgerLine"></div>
                 </label>
                 <nav>
-                <ul>
-                    <li><a href="#">Homepage</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Prices</a></li>
-                </ul>
+                    <ul>
+                        <li><a href="#">Homepage</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Prices</a></li>
+                    </ul>
                 </nav>
-                <div className="page-shader"></div>
             </div>
         </header>
     )
